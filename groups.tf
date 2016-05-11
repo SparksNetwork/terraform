d2bf8942f152aaa458bb3741646f2be24a07c130
Jeremy Wells <jemmyw@gmail.com>
2016-05-11 22:15:49 +1200

    Add engineers group

--- groups.tf
+++ groups.tf
@@ -1,5 +1,21 @@
 resource "aws_iam_group" "admin" {
-    name = "admin"
-    path = "/"
+  name = "admin"
+  path = "/"
 }
 
+resource "aws_iam_group_membership" "admin" {
+  name  = "admin-group-membership"
+  users = ["jwells", "tsteinberger", "sdebaun"]
+  group = "${aws_iam_group.admin.name}"
+}
+
+resource "aws_iam_group" "engineers" {
+  name = "engineers"
+  path = "/"
+}
+
+resource "aws_iam_group_membership" "engineers" {
+  name = "engineers-group-membership"
+  users = ["jwells", "tsteinberger", "sdebaun"]
+  group = "${aws_iam_group.engineers.name}"
+}
