36509b90e5989160ad3816c20f7735c9ef6cc262
Jeremy Wells <jemmyw@gmail.com>
2016-09-25 12:07:25 +1300

    Add terraform user policy

--- modules/terraform/iam.tf
+++ modules/terraform/iam.tf
@@ -3,3 +3,22 @@ resource "aws_iam_user" "terraform" {
   name = "terraform"
   path = "/"
 }
+
+resource "aws_iam_user_policy" "terraform" {
+  name = "terraform"
+  user = "${aws_iam_user.terraform.name}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
