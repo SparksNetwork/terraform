3422e452693dbad81f896f9671c25f7cc34fb376
Jeremy Wells <jemmyw@gmail.com>
2016-09-25 21:12:25 +1300

    Shutdown the lambda performance test

--- lambda-performance.tf
+++ lambda-performance.tf
@@ -1,16 +1,16 @@
-variable "apex_function_sns-consumer" {}
-variable "apex_function_kinesis-consumer" {}
-
-module "lambda-performance" {
-  source = "./modules/lambda-performance"
-  sns_consumer_arn = "${var.apex_function_sns-consumer}"
-  kinesis_consumer_arn = "${var.apex_function_kinesis-consumer}"
-}
-
-output "lp_sns_topic_arn" {
-  value = "${module.lambda-performance.sns_topic_arn}"
-}
-
-output "lp_sns_dynamodb_table_arn" {
-  value = "${module.lambda-performance.sns_dynamodb_table_arn}"
-}
+//variable "apex_function_sns-consumer" {}
+//variable "apex_function_kinesis-consumer" {}
+//
+//module "lambda-performance" {
+//  source = "./modules/lambda-performance"
+//  sns_consumer_arn = "${var.apex_function_sns-consumer}"
+//  kinesis_consumer_arn = "${var.apex_function_kinesis-consumer}"
+//}
+//
+//output "lp_sns_topic_arn" {
+//  value = "${module.lambda-performance.sns_topic_arn}"
+//}
+//
+//output "lp_sns_dynamodb_table_arn" {
+//  value = "${module.lambda-performance.sns_dynamodb_table_arn}"
+//}
